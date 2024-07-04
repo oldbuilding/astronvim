97c167ecb8b8effa14fa10ffc17ac9a38d67d315
Miguel Martin <m42martin+@gmail.com>
2024-07-04 10:32:38 -0400

    kanagawa compile not correct yet

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "Verf/deepwhite.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      return {
+        theme = "deepwhite",
+        low_blue_light = true,
+      }
+    end,
+    config = function(_, opts)
+      require("deepwhite").setup(opts)
+      vim.cmd.colorscheme("deepwhite")
+    end,
+  },
+  { "savq/melange-nvim" },
+  { "SebastianZaha/nvim-solar-paper" },
+  {
     "nyoom-engineering/oxocarbon.nvim",
     -- Add in any other configuration;
     --   event = foo,
